gdvenugopal88/pytest-regtest

commit e76000b4e8301ea735e5fcf40cf84e3e51c8fdfc

make pytest-regtest Python3 compatible

pytest_regtest.py
@@ -6,7 +6,11 @@ This plugin enables recording of ouput of testfunctions which can be compared on
 runs.
 """
 
-import cStringIO
+try:
+    import cStringIO.StringIO as StringIO
+except ImportError:
+    from io import StringIO
+
 import contextlib
 import difflib
 import os
@@ -80,7 +84,7 @@ class Tee(object):
 @pytest.yield_fixture()
 def regtest(request):
 
-    fp = cStringIO.StringIO()
+    fp = StringIO()
     if tee:
         fp = Tee(fp)
 
@@ -92,7 +96,7 @@ def regtest(request):
 @pytest.yield_fixture()
 def regtest_redirect(request):
 
-    fp = cStringIO.StringIO()
+    fp = StringIO()
     if tee:
         fp = Tee(fp)
 
@@ -117,7 +121,7 @@ def regtest_redirect(request):
 @pytest.yield_fixture()
 def regtest_capture_all(request):
 
-    fp = cStringIO.StringIO()
+    fp = StringIO()
 
     import sys
     old = sys.stdout

tests/test_00.py
@@ -1,21 +1,29 @@
+from __future__ import print_function
+
+
 def test0(regtest):
-    print "test0"
-    print >> regtest, "this is the expected output\t"
+    print("test0")
+    print("this is the expected output\t", file=regtest)
+
 
 def test1(regtest):
-    print "test1"
-    print >> regtest, "number one !!"
+    print("test1")
+    print("number one !!", file=regtest)
     assert False
+
 
 def test2():
     assert True
 
+
 def test3():
     assert False
 
+
 def test4(regtest_redirect):
     with regtest_redirect():
-        print "hi"
+        print("hi")
+
 
 def test_with_object_id(regtest):
-    print >> regtest, test_with_object_id
+    print (test_with_object_id, file=regtest)

commit 9bde687c55953703f5ff7c3cee671866c04750d5

fixed regression from e76000b4e83

pytest_regtest.py
@@ -7,7 +7,7 @@ runs.
 """
 
 try:
-    import cStringIO.StringIO as StringIO
+    from cStringIO import StringIO
 except ImportError:
     from io import StringIO
 