1c1b0df111b6fffd61d49480a0f04ff171efccaa
Uwe Schmitt <uwe.schmitt@id.ethz.ch>
2018-08-06 11:28:40 +0200

    bumped version to 1.3.0

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from __future__ import print_function
 
 import os
 import sys
+
 from setuptools import setup
 
-VERSION = (1, 2, 1)
+VERSION = (1, 3, 0)
 
 AUTHOR = "Uwe Schmitt"
 AUTHOR_EMAIL = "uwe.schmitt@id.ethz.ch"
@@ -24,8 +25,9 @@ if len(sys.argv) > 1 and "dist" in sys.argv[1] and "wheel" not in sys.argv[1]:
 
     here = os.path.dirname(os.path.abspath(__file__))
     try:
-        rst_content = check_output("pandoc {} -t rst".format(os.path.join(here, "README.md")),
-                                shell=True)
+        rst_content = check_output(
+            "pandoc {} -t rst".format(os.path.join(here, "README.md")), shell=True
+        )
 
         LONG_DESCRIPTION = str(rst_content, encoding="ascii")
 
@@ -38,19 +40,14 @@ if __name__ == "__main__":
     setup(
         version="%d.%d.%d" % VERSION,
         name="pytest-regtest",
-        py_modules=['pytest_regtest'],
+        py_modules=["pytest_regtest"],
         author=AUTHOR,
         author_email=AUTHOR_EMAIL,
         url=URL,
         license=LICENSE,
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
-
         # the following makes a plugin available to pytest
-        entry_points={
-            'pytest11': [
-                'regtest = pytest_regtest',
-            ]
-        },
+        entry_points={"pytest11": ["regtest = pytest_regtest"]},
         install_requires=["pytest>=3.3.2"],
     )
